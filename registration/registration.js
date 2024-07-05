5e531f9b08b72e07d174872e6d12a9d01c4adf6f
Daniel Yoon <daniel.yoon1208@gmail.com>
2024-07-06 06:30:16 +0900

    completed registration page html/css skeleton w/ block display

---
+++ registration/registration.js
@@ -0,0 +1,16 @@
+function toggleTab(tabIndex) {
+    let tabs = document.getElementsByClassName("tab");
+    for (let i=0; i<tabs.length; i++) {
+        tabs[i].style.display = "none";
+    }
+    tabs[tabIndex].style.display = "block";
+
+    // remove all active tabs
+    let buttons = document.getElementsByClassName("tab-button");
+    for (let i=0; i<tabs.length; i++) {
+        buttons[i].classList.remove("active-tab-button");
+    }
+    
+    // set active tab
+    buttons[tabIndex].classList.add("active-tab-button");
+}
