7fe5f8cf55af407f80bf76329a5fb675f82b72f1
Daniel Yoon <daniel.yoon1208@gmail.com>
2024-07-28 23:29:32 +0900

    basic registration fetch req from vanilla js, not sure about security, likely migrate to react

--- server/public/js/registration.js
+++ server/public/js/registration.js
@@ -16,3 +16,36 @@ function toggleTab(tabIndex) {
 }
 
 toggleTab(0);
+
+document.querySelector('#signup-form').addEventListener('submit', submitSignup);
+
+async function submitSignup(e) {
+    e.preventDefault();
+    const username = document.querySelector("#user-text").value;
+    const stuEmail = document.querySelector("#signup-email-text").value;
+    const password = document.querySelector("#signup-pass").value;
+    const confPwd = document.querySelector("#conf-pass").value;
+
+    if (password === confPwd) {
+        const res = await fetch('/register', {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                user: username,
+                pwd: password,
+                email: stuEmail
+            })
+        });
+
+        if (res.status === 201) {
+            alert('Success');
+        } else if (res.status === 409) {
+            alert("Username or email already exists");
+        }
+    } else {
+        alert("Password does not match confirmation");
+    }
+}
+
